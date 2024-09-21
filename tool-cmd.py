acbb0ce5ac8b818d2288e26a3c37bfe7c6953e40
Z0KRI <david1166@live.com.mx>
2024-09-21 01:26:30 -0600

    ⚡️ Feat: CMD V1.1

--- tool-cmd.py
+++ tool-cmd.py
@@ -1,17 +1,24 @@
 import argparse
 from src.core.const import *
+from src.core.utils.revolve_args import *
+from src.core.core import Core
 
 class ToolCMD:
-    description = 'Descripción de la herramienta'
+    name = 'address-sql'
+    description = """Herramienta para automatizar el
+    proceso de migraciones de los estados, ciudades y códigos postales"""
 
     def setArgs(self, parser: argparse.ArgumentParser):
-        parser.add_argument('--type', type=str, choices=['csv', 'sql'], help=TYPE_HELP)
+        parser.add_argument('-t', '--type', type=str, choices=['csv', 'sql'], default='sql', help=TYPE_HELP)
+        parser.add_argument('-of', '--only-file', help=FILE_HELP, action='store_true')
+        parser.add_argument('-p', '--path', help=PATH_HELP, default=r'src\files\csv')
         self.args = self.parser.parse_args()
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description=self.description)
+        self.parser = argparse.ArgumentParser(prog=self.name, description=self.description)
         self.setArgs(self.parser)
-        print(self.args)
+        self.args = ResolveArgs.handler(self.args)
+        Core(self.args)
 
 if __name__ == "__main__":
     tool = ToolCMD()
