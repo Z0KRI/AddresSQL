2beb9eea0c6f7a2ef106e52c9f045135e29a816a
Z0KRI <david1166@live.com.mx>
2024-09-20 20:23:59 -0600

    ✏️ Feat: File Help

--- src/core/const/help.py
+++ src/core/const/help.py
@@ -1,1 +1,2 @@
 TYPE_HELP = 'Este parámetro hace referencia a que tipo de archivo quiere de como salida (csv, sql)'
+FILE_HELP = 'Este parámetro sirve para determinar si quieres un solo archivo de migración o varios'
