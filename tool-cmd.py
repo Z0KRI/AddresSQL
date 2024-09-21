2a5df6e1223d52b0c876dd0de9e5d389f5bcfdee
Z0KRI <david1166@live.com.mx>
2024-09-21 03:58:43 -0600

    ⚡️ Feat: Agregando argumentos
    Se agregan los argumentos para una carpeta de salida y un engine en caso de  usar sql

--- tool-cmd.py
+++ tool-cmd.py
@@ -12,6 +12,8 @@ class ToolCMD:
         parser.add_argument('-t', '--type', type=str, choices=['csv', 'sql'], default='sql', help=TYPE_HELP)
         parser.add_argument('-of', '--only-file', help=FILE_HELP, action='store_true')
         parser.add_argument('-p', '--path', help=PATH_HELP, default=r'src\files\csv')
+        parser.add_argument('-o', '--output', help=OUT_PUT_HELP, default=r'src\exports')
+        parser.add_argument('-e', '--engine', help=ENGINE, type=str, choices=['mysql', 'postgres'], default='postgres')
         self.args = self.parser.parse_args()
 
     def __init__(self):
